e5b4e8b9d2a034d22137e45e0c9e777353abe163
Phenny <mwaisakaphenny@gmail.com>
2024-12-03 12:02:39 +0300

    Random Password Generator

--- RandomPassword.java
+++ RandomPassword.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class RandomPassword {
@@ -9,13 +10,38 @@ public class RandomPassword {
         String numbers = "0123456789";
         String specialChars = "!@#$%^&*()-_=+[]{}|;:,.<>?/";
 
-        while(){
+        while(true){
             System.out.println("This a Random Password Generator.");
             System.out.print("Enter the desired password length: ");
-            int length = scanner.nextInt();
-            System.out.println("Generating a password...");
-            System.out.print("I've included numbers, special characters, uppercase and lowercase letters");
-            System.out.println("Generated Password: " + password);
+            int passwordLength = scanner.nextInt();
+            scanner.nextLine();
+
+            StringBuilder charPool = new StringBuilder();
+            charPool.append(lowercase).append(uppercase).append(numbers).append(specialChars);
+
+            String password = generatePassword(charPool.toString(), passwordLength);
+            System.out.println("Generated password: " + password);
+
+            System.out.print("Generate another password? (yes/no): ");
+            String choice = scanner.nextLine();
+            if (!choice.equalsIgnoreCase("yes")){
+                System.out.println("Okay....byee!");
+                break;
+            }
         }
+        scanner.close();
+    }
+
+    //Method to generate password
+    public static String generatePassword(String charPool, int length){
+        Random random = new Random();
+        StringBuilder password = new StringBuilder();
+
+        for (int i = 0; i < length; i++){
+            int randomIndex = random.nextInt(charPool.length());//explain this line
+            password.append(charPool.charAt(randomIndex));
+        }
+
+        return password.toString();
     }
 }
---
+++ out/production/JavaProjects/springboot-innovatorsclub-backend/README.md
@@ -0,0 +1,22 @@
+## Meru Innovators Backend application
+- tech is our home and we should embrace it 😄 ⭐
+## How to get started
+- Ensure you create a file named `keys.properties` inside `resource` folder
+- here are what you will add
+  ```properties
+  ## google secrets
+  google.clientId = your_google_client_id
+  google.clientSecret= your_google_client_secret
+
+  ##Database keys(for this project we are using postgres)
+  
+  db.url = database_url_you_are_using
+  db.password=database_password
+  db.username = database_username
+
+  ## mail secrets
+  mail.username = email_for_sending_emails
+  mail.password = email_password
+  mail.host = your_host
+  mail.port = email_port
+  ```
